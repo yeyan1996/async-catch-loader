173ea5639a933de6b74d4d75b97e4b8e6f0b7f9a
yeyan1996 <1366792281@qq.com>
2019-07-08 10:45:24 +0800

    ver:1.0.0

--- README.md
+++ README.md
@@ -1,1 +1,34 @@
 # async-catch-loader
+一个自动给 async 函数注入 try/catch 的 webpack loader
+
+在开发中经常会使用 async/await 异步编程，同时也会频繁的使用 try/catch 捕获异步中的错误，使得业务代码充斥这 try/catch 非常的冗余，使用这个 loader 可以在打包后的代码自动注入 try/catch,使得业务代码非常简洁
+
+```javascript
+async function func() {
+    let res = await new Promise(resolve => {
+        setTimeout(() => {
+            resolve('success')
+        }, 3000)
+    })
+}
+```
+
+打包后自动注入 try/catch
+```javascript
+async function func() {
+    try {
+       let res = await new Promise(resolve => {
+            setTimeout(() => {
+                resolve('success');
+            }, 3000);
+        });
+    } catch (e) {//...}
+}
+```
+
+## Api
+|Name|Type|Description|
+|:--:|:--:|:----------|
+|**`catchCode`**|`{String}`|`catch 子句中的代码片段，错误对象的变量名为 e`
+
+
---
+++ index.js
@@ -0,0 +1,63 @@
+const parser = require("@babel/parser")
+const traverse = require("@babel/traverse").default
+const t = require("@babel/types")
+const core = require("@babel/core")
+const loaderUtils = require("loader-utils");
+
+const DEFAULT = {
+    cacheCode: `console.warn(e)`
+}
+
+module.exports = function (source) {
+    let options = loaderUtils.getOptions(this)
+    let ast = parser.parse(source)
+    options = {
+        DEFAULT,
+        ...options,
+    }
+    let catchBody = []
+    let catchAst = parser.parse(options.catchCode)
+    catchBody = catchAst.program.body
+    traverse(ast, {
+        AwaitExpression(path) {
+            if (path.node.done) return
+            path.node.done = true //防止 AwaitExpression 无限递归导致栈溢出
+
+            let identifierAst = t.identifier('e')
+            if (t.isVariableDeclarator(path.parent)) { // 变量声明
+                let variableDeclarationPath = path.parentPath.parentPath
+                let tryCatchAst = t.tryStatement(
+                    t.blockStatement([
+                        variableDeclarationPath.node // Ast
+                    ]),
+                    t.catchClause(identifierAst, t.blockStatement(catchBody))
+                )
+                variableDeclarationPath.replaceWithMultiple([
+                    tryCatchAst
+                ])
+            } else if (t.isAssignmentExpression(path.parent)) { // 赋值表达式
+                let expressionStatementPath = path.parentPath.parentPath
+                let tryCatchAst = t.tryStatement(
+                    t.blockStatement([
+                        expressionStatementPath.node
+                    ]),
+                    t.catchClause(identifierAst, t.blockStatement(catchBody))
+                )
+                expressionStatementPath.replaceWithMultiple([
+                    tryCatchAst
+                ])
+            } else { // await 表达式
+                let tryCatchAst = t.tryStatement(
+                    t.blockStatement([
+                        t.expressionStatement(path.node)
+                    ]),
+                    t.catchClause(identifierAst, t.blockStatement(catchBody))
+                )
+                path.replaceWithMultiple([
+                    tryCatchAst
+                ])
+            }
+        }
+    })
+    return core.transformFromAst(ast).code
+}
